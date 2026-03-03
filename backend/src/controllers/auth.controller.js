61004179fd2c5a0f9cdf094477ad866a48a928ca
KiranKumar01-01 <kirank75884@gmail.com>
2026-03-03 15:32:42 +0530

    welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { ENV } from "../lib/env.js";
+
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -39,8 +42,11 @@ export const signup = async (req, res) => {
             password: hashedPassword
         });
         if(newUser){
-            generateToken(newUser._id,res);
-        await newUser.save();
+        //     generateToken(newUser._id,res);
+        // await newUser.save();
+
+        const savedUser=await newUser.save();
+        generateToken(savedUser._id,res)
 
         res.status(201).json({
             _id: newUser._id,
@@ -48,7 +54,13 @@ export const signup = async (req, res) => {
             email: newUser.email,
             profilePic: newUser.profilePic,
         });
-        //todo:send a welcome email to user
+        //send a welcome email to user
+        try{
+            await sendWelcomeEmail(savedUser.email,savedUser.fullName,ENV.CLIENT_URL)
+        }catch(error){
+            console.error("Failed to send welcome email:",email)
+        }
+
         }else{
             res.status(400).jsom({message:"Invalid user data"})
         }
---
+++ backend/src/emails/emailHandlers.js
@@ -0,0 +1,24 @@
+import { resendClient, sender } from "../lib/resend.js";
+import { createWelcomeEmailTemplate } from "../emails/emailTemplates.js";
+
+export const sendWelcomeEmail = async (email, name, clientURL) => {
+  try {
+    const { data, error } = await resendClient.emails.send({
+      from: `${sender.name} <${sender.email}>`,
+      to: email,
+      subject: "Welcome to Chatify",
+      html: createWelcomeEmailTemplate(name, clientURL),
+    });
+
+    if (error) {
+      console.error("Error sending welcome email:", error);
+      throw new Error("Failed to send welcome email");
+    }
+
+    console.log("Welcome Email sent successfully", data);
+    return data;
+
+  } catch (err) {
+    console.error("Failed to send welcome email:", err.message);
+  }
+};
