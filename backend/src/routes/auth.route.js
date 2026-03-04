fff72b6a595f68ba12038c7418e98094f3f4ecb2
KiranKumar01-01 <kirank75884@gmail.com>
2026-03-04 17:13:18 +0530

    rate limit implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,15 @@
 import express from "express";
 import { signup, login, logout , updateProfile } from "../controllers/auth.controller.js";
 import {protectRoute} from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../models/arcjet.middleware.js";
 const router=express.Router();
 
-router.post("/signup", signup)
-
-router.post("/login",login)
-
-router.post("/logout",logout)
-
+router.post("/signup", signup);
+router.post("/login", login)
+router.post("/logout", logout)
 router.get("/check",protectRoute,(req,res)=>res.status(200).json(req.user))
 
-router.put("/update-profile",protectRoute,updateProfile) 
+router.put("/update-profile", protectRoute,updateProfile) 
 //as thz is updte we use the put
   // if only user is authenticted next funcyion is called
 
