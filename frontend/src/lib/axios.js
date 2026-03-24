76351da2ad640d9b0b9e44e3ce3514bb48b1ab95
KiranKumar01-01 <kirank75884@gmail.com>
2026-03-24 17:38:20 +0530

    fix axios baseURL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,11 @@
-import  axios  from "axios";
+import axios from "axios";
 
-export const axiosInstance=axios.create({
-    baseURL: import.meta.env.MODE ==="development" ? "http://localhost:3000/api" : "/api",
-    withCredentials:true,   //send the cookies
-})
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : `${import.meta.env.VITE_BASE_URL}/api`;
 
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
