648ad97cb8e8ed5971e891d25a0251c5797e4a16
KiranKumar01-01 <kirank75884@gmail.com>
2026-03-24 17:44:16 +0530

    FIX VERCEL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000/api"
-    : `${import.meta.env.VITE_BASE_URL}/api`;
+    : `${import.meta.env.VITE_BASE_URL || "https://realtime-chat-nleh.onrender.com"}/api`;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
