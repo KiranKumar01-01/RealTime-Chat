594d6049b5cfbca5f07ed9c0c12ae67a7de1b3a1
kirangowdaa01 <1by23cs105@gmail.com>
2026-06-19 18:12:57 +0530

    Updated socket CORS and email configuration

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -33,11 +33,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://real-time-chat-psi-mauve.vercel.app",
-    ],
-    methods: ["GET", "POST"],
+    origin: [ENV.CLIENT_URL],
     credentials: true,
   },
 });
